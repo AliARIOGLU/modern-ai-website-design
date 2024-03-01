4b5ffa6f7d16da0cfc8439f3d098958c1365758e
Ali <ali.arioglu06@gmail.com>
2024-03-01 21:47:56 +0300

    benefit section

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -6,7 +6,7 @@ export const CompanyLogos = ({ className }) => {
       <h5 className="tagline mb-6 text-center text-n-1/50">
         Helping people create beautiful content at
       </h5>
-      // TODO: this list should have infinite x-scroll animation
+      {/* TODO: this list should have infinite x-scroll animation */}
       <ul className="flex">
         {companyLogos.map((logo, index) => (
           <li
